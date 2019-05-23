bd64b946852c883d6bfd2649866f06a0c11b1e24
fribsee <40807715+Fribsee@users.noreply.github.com>
2019-05-22 19:44:11 -0500

    s

--- src/app/links/links.component.ts
+++ src/app/links/links.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { BooksearchService } from 'src/app/booksearch.service';
+import { BooksearchService } from '../booksearch.service';
 
 @Component({
   selector: 'app-links',
--- src/app/search-results/search-results.component.ts
+++ src/app/search-results/search-results.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { BooksearchService } from 'src/app/booksearch.service';
+import { BooksearchService } from '../booksearch.service';
 import { switchMap } from 'rxjs/operators';
 
 
